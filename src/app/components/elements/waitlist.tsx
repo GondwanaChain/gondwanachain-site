480fa94ccd4c3829d30ddd6230729194e3fab56a
CAFQ-Dev <websmith@anvil-dev.site>
2025-10-01 08:15:10 +0200

    vercel

--- src/app/components/elements/waitlist.tsx
+++ src/app/components/elements/waitlist.tsx
@@ -3,7 +3,7 @@ export default function Waitlist() {
   return (
   <div className="min-h-[80vh] min-w-[20vw]"> <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeftB6Z2_H5J6EBYm_z_3xg0SzSN9LEvUdvth7yt-L4rYoXwQ/viewform?usp=header"
         frameBorder="0"
-        allowFullScreen className="min-h-[80vh] min-w-[34vw]">Loading...</iframe>
+        allowFullScreen className="min-h-[80vh] min-w-[33vw]">Loading...</iframe>
   </div>
   );
 }
