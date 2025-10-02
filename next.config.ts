1777925f8f8ac8335a813f73f10dbc0c4b98021b
CAFQ-Dev <technical@coinafriq>
2025-10-02 14:10:47 +0200

    Zod fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    transpilePackages: [
+    '@tanstack/react-form', 
+    '@tanstack/zod-form-adapter'
+  ],
 };
 
 export default nextConfig;
