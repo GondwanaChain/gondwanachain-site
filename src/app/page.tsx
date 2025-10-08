e0db3a2863f6133a61280e95aebe7a40ae6e14f9
CAFQ-Dev <websmith@anvil-dev.site>
2025-10-08 10:30:41 +0200

    fix socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 		<section className="bg-base-200 flex items-center justify-center">
 			<Subscribe />
 		</section>
-		<section className="bg-base-200 flex items-center justify-between px-56 pt-16 pb-4">
+		<section className="bg-base-200 flex flex-col lg:flex-row items-center justify-center lg:justify-between px-56 pt-16 pb-4">
 		<p className="text-lg">Copyright &copy; 2025. GondwanaChain LLC. All rights reserved.</p>
 		<Socials />
 		</section>
