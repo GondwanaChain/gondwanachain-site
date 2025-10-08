5ce2f409c363333c9efaad2d34fa2ee9f8ab14fd
CAFQ-Dev <websmith@anvil-dev.site>
2025-10-08 15:51:57 +0200

    basic animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "GondanaChain",
+  title: "GondwanaChain",
   description: "Trust, Transparency and Efficiency on the Chain",
 };
 
